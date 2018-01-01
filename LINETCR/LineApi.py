734bb971e367c11bf0753d9b47fd5690246439b3
shillbie <33694875+shillbie@users.noreply.github.com>
2018-01-01 18:19:15 +0700

    Create LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -48,14 +48,14 @@ class LINE:
               'X-Line-Access': self.authToken, 
               'User-Agent': 'Line/7.14.0'
    }
-    self.Poll = Poll(self.authToken)
-    self.channel = channel.Channel(self.authToken)
-    self.channel.login()	
-    self.mid = self.channel.mid
-    self.channel_access_token = self.channel.channel_access_token
-    self.token = self.channel.token
-    self.obs_token = self.channel.obs_token
-    self.refresh_token = self.channel.refresh_token
+    #self.Poll = Poll(self.authToken)
+    #self.channel = channel.Channel(self.authToken)
+    #self.channel.login()	
+    #self.mid = self.channel.mid
+    #self.channel_access_token = self.channel.channel_access_token
+    #self.token = self.channel.token
+    #self.obs_token = self.channel.obs_token
+    #self.refresh_token = self.channel.refresh_token
 
 
   """User"""
